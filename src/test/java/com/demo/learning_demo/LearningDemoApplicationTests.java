1713c30c973acd84975a107382c53296abfd9245
zhou <13052172963@163.comm>
2023-11-22 21:39:43 +0800

    commit project

---
+++ src/test/java/com/demo/learning_demo/LearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.learning_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
